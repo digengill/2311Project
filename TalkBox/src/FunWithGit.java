f974751212f92dd41a56f7c3eccee0221ae17af5
digengill <gilldigen@gmail.com>
2019-01-10 11:49:57 -0500

    try 2

--- TalkBox/src/FunWithGit.java
+++ TalkBox/src/FunWithGit.java
@@ -3,7 +3,7 @@ public class FunWithGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Fun with git");
+System.out.println("Fun with gitmmm");
 	}
 
 }
