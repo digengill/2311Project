b3c66f8896847712a41b4cf590b0e1be11eb56be
digengill <gilldigen@gmail.com>
2019-01-14 12:26:33 -0500

    d

--- TalkBox/src/FunWithGit.java
+++ TalkBox/src/FunWithGit.java
@@ -3,7 +3,10 @@ public class FunWithGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Fun with github edit by Digdfenedwdw");
+
+
+System.out.println("Fun with git-usama1");
+
 	}
 
 }
--- TalkBox/src/practiceUsama.java
+++ TalkBox/src/practiceUsama.java
@@ -3,7 +3,7 @@ public class practiceUsama {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print(true);
+		System.out.print(" https://www.eecs.yorku.ca/course_archive/2017-18/W/1021/slides/GUIs.pdf");
 
 	}
 
