39de9acb4d511b971f77f6c9b6fe107a3ca0cc94
Satinder-Sikand <ssatin360@gmail.com>
2019-01-28 10:54:37 -0500

    Updated Files for Speaker class, TalkBox simulator and libraries

---
+++ TalkBox/src/SpeakerClass.java
@@ -0,0 +1,59 @@
+// Speaker Shit  
+// text to speech 
+import java.util.Locale; 
+import javax.speech.Central; 
+import javax.speech.synthesis.Synthesizer; 
+import javax.speech.synthesis.SynthesizerModeDesc; 
+//Scanner from file
+import java.io.*;
+import java.util.*;
+
+public class SpeakerClass {
+	
+	    public SpeakerClass (String btn)  throws Exception
+	    { 
+	    	
+	    	File file = new File(btn);
+	    	Scanner scanner  = new Scanner(file);
+	  
+
+            // set property as Kevin Dictionary 
+            System.setProperty("freetts.voices", 
+                "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");  
+                  
+            // Register Engine 
+            Central.registerEngineCentral 
+                ("com.sun.speech.freetts.jsapi.FreeTTSEngineCentral");  
+  
+            // Create a Synthesizer 
+            Synthesizer synthesizer =                                          
+                Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));      
+      
+            // Allocate synthesizer 
+            synthesizer.allocate();  
+              
+            // Resume Synthesizer 
+            synthesizer.resume(); 
+            
+	        try 
+	        {     
+	              
+	            while(scanner.hasNextLine()) {
+	            	String talk = scanner.nextLine();
+	            	 // speaks the given text until queue is empty. 
+	            	synthesizer.speakPlainText(talk, null);          
+	            	synthesizer.waitEngineState(Synthesizer.QUEUE_EMPTY); 
+	              
+	            	// Deallocate the Synthesizer. 
+	            	//synthesizer.deallocate(); 
+	            }
+	                                            
+	        }  
+	  
+	        catch (Exception e)  
+	        { 
+	            e.printStackTrace(); 
+	        } 
+	    } 
+	} 
+
