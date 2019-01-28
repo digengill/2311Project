f3d91238da05fd55425930e3c690f7fa0a5ac1d8
digengill <gilldigen@gmail.com>
2019-01-28 13:23:06 -0500

    test

--- TalkBox/src/TalkBoxSimulatorTest.java
+++ TalkBox/src/TalkBoxSimulatorTest.java
@@ -1,12 +1,16 @@
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TalkBoxSimulatorTest {
+	
+	TalkBoxSimulator tbb;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		tbb = new TalkBoxSimulator();
 	}
 
 	@Test
@@ -20,5 +24,10 @@ class TalkBoxSimulatorTest {
 		fail("Not yet implemented");
 	}
 	@Test
-	test
+	void isButtonStringEmpty()
+	{
+		
+		assertTrue(1,tbb.isButtonStringEmpty(1));
+		
+	}
 }
