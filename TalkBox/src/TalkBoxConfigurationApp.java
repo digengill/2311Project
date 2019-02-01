7a7d7640b07ac51abd76f8aaafc0e864cc89a272
UsamaAbdali <usama01@my.yorku.ca>
2019-02-01 14:01:27 -0500

    method to implement over the weekend

--- TalkBox/src/TalkBoxConfigurationApp.java
+++ TalkBox/src/TalkBoxConfigurationApp.java
@@ -1,9 +1,36 @@
+import java.io.File;
 
 public class TalkBoxConfigurationApp {
 
+	
+	
+	
+	String b1Input;
+	String b2Input;
+	String b3Input;
+	String b4Input;
+	String b5Input;
+	String b6Input;
+	
+	
+	
+	
+	String imgName(String userButtonInput) {
+		return null;
+	}
+	
+	//method to choose button and write content for that button
+	void writeToFile(String fileContent, String buttonFile) {
+		File f1= new File("");
+	}
+	 
+	 
+		
 	TalkBoxSimulator talk1= new TalkBoxSimulator();
 	TalkBoxSimulator talk2= new TalkBoxSimulator("sad","","","","","");
-	
+	TalkBoxSimulator talk3= new TalkBoxSimulator("sad","Nice","","","","");
+
+		
 	
 	
 	
