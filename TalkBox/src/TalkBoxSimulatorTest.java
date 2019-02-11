425692381afe3273c54896c6cae671622004aca1
UsamaAbdali <usama01@my.yorku.ca>
2019-02-11 02:48:50 -0500

    Update to the tester classes

--- TalkBox/src/TalkBoxSimulatorTest.java
+++ TalkBox/src/TalkBoxSimulatorTest.java
@@ -1,8 +1,12 @@
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.swing.JFrame;
+
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 
 class TalkBoxSimulatorTest {
 	
@@ -11,23 +15,30 @@ class TalkBoxSimulatorTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		tbb = new TalkBoxSimulator();
+		tbb.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		tbb.setTitle("Application Test");
+		tbb.pack();
+        tbb.setVisible(true);
 	}
+	@Test
+	public void testOfAllButtons() throws InterruptedException{
+		tbb.b1.doClick();
+		tbb.b2.doClick();
+		tbb.b3.doClick();
+		tbb.b4.doClick();
+		tbb.b5.doClick();
+		tbb.b6.doClick();
+	}
+	
+//	@Test
+//	void test() {
+//		TalkBoxSimulator talk1= new TalkBoxSimulator("","","","","","");
+//		boolean result= talk1.isButtonStringEmpty(1);
+//		
+//		assertTrue(false==result);
+//
+//
+//	}
 
-	@Test
-	void testTalkBoxSimulator() {
-	fail("Not yet implemented");
-		
-	}
 
-	@Test
-	void testActionPerformed() {
-		fail("Not yet implemented");
-	}
-	@Test
-	void isButtonStringEmpty()
-	{
-		fail("Not implemented yet");
-	//	assertTrue(true == tbb.isButtonStringEmpty(7));
-		
-	}
 }
