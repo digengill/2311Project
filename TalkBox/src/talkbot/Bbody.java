a2b3c40360f47625f48707837a73c885f3b04f8d
HP USER <HP USER@DESKTOP-HVLV4NG>
2019-03-30 15:50:56 -0400

    BBody and configuration to allow for add remove set code and ArrayList

--- TalkBox/src/talkbot/Bbody.java
+++ TalkBox/src/talkbot/Bbody.java
@@ -14,6 +14,11 @@ import java.util.ArrayList;
 
 import sun.audio.*;
 
+/*class Set{
+	String name;
+	ArrayList<Buttons> track = new ArrayList<Buttons>();
+	
+}*/
 public class Bbody extends JPanel implements ActionListener {
 	
 	
@@ -22,9 +27,11 @@ public class Bbody extends JPanel implements ActionListener {
 	private JScrollPane scroll;
 	protected Configuration con;
 	private ObjectInputStream in;
-	public JButton set1, set2;
-	public ArrayList<ArrayList<Buttons>> sets;
-	public ArrayList<Buttons> track1, track2;
+	private ArrayList<JButton> categories;
+	//public JButton set1, set2;
+	//public ArrayList<ArrayList<Buttons>> sets;//track1, track2;
+	private ArrayList<ArrayList<Buttons>> tbuttons;
+	private int currentset=0;
 	private Boolean on, play=false;
 	private Clip clip;
 	 
@@ -35,48 +42,57 @@ public class Bbody extends JPanel implements ActionListener {
 		sp.setMaximumSize(new Dimension(this.getWidth(), 300));
 		sp.setBackground(new Color(255, 204, 209));
 		sp.setLayout(new FlowLayout());
+		categories = new ArrayList<JButton>();
 	}
 	
 	public void sets() {
-//		for(int i = 0; i <= con.getNumberOfAudioSets(); i++)
-//		{
-//			JButton Button = new JButton(con.getSetName(i));
-//			this.add(Button);
-//			Button.addActionListener(this);
-//		}
-			
-		set1 = new JButton("Phrases");
-		set2 = new JButton("Emotions");	
-		this.add(set1);
-		this.add(set2);
-		set1.addActionListener(this);
-		set2.addActionListener(this);
+		for (int i = 0; i < con.getNumberOfAudioSets(); i++) {
+			System.out.println(i + "\there");
+			JButton temp = new JButton(con.getcatNames(i));System.out.println(con.getNumberOfAudioSets());
+			categories.add(temp);
+		} 
+		//set1 = new JButton("Phrases");
+		//set2 = new JButton("Emotions");
+		for (int i = 0; i < categories.size(); i++) {
+			this.add(categories.get(i));
+			this.categories.get(i).addActionListener(this);
+		}
+		//this.add(set1);
+		//this.add(set2);
+		//set1.addActionListener(this);
+		//set2.addActionListener(this);
 	}
 	
 	public void setbtns() {//
-		track1 = new ArrayList<Buttons>();
-		track2 = new ArrayList<Buttons>(); on=true;
-		for (int h = 0; h < 2; h++) {
+		tbuttons = new ArrayList<ArrayList<Buttons>>();
+		
+		on=true;
+		for (int h = 0; h < con.getNumberOfAudioSets(); h++) {
 			if (h==0) { //System.out.println(con.getSet1());
-				for (int i = 0; i < con.getSet1(); i++) {
+				ArrayList<Buttons> temp = new ArrayList<Buttons>();
+				for (int i = 0; i < con.getSetAt(h); i++) {
 					//System.out.println(con.getPathToImageFile(1, i+1)); System.out.println(i);
-					Buttons btn = new Buttons(con.getBtnName(1, i+1), con.getPathToImageFile(1, i+1), i+1);
+					Buttons btn = new Buttons(con.getBtnName(h+1, i+1), con.getPathToImageFile(h+1, i+1), i+1);
 					btn.addActionListener(this);
-					track1.add(btn);
-					sp.add(track1.get(i));
+					temp.add(btn);
+					
+					sp.add(temp.get(i));
 					this.revalidate();
 					this.repaint();
 					//////////////////////MAYBE SURROUND WITH TRY CATCH
 				}
-			} else if (h==1) {
-				for (int i = 0; i < con.getSet2(); i++) {//System.out.println(con.getPathToImageFile(2, i+1));
-					Buttons btn = new Buttons(con.getBtnName(2, i+1), con.getPathToImageFile(2, i+1), i+1);
+				tbuttons.add(temp);
+			} else {
+				ArrayList<Buttons> temp = new ArrayList<Buttons>();
+				for (int i = 0; i < con.getSetAt(h); i++) {//System.out.println(con.getPathToImageFile(2, i+1));
+					Buttons btn = new Buttons(con.getBtnName(h+1, i+1), con.getPathToImageFile(h+1, i+1), i+1);
 					//btn.addActionListener(this);
-					track2.add(btn);
-					sp.add(track2.get(i));
-					track2.get(i).setVisible(false);
+					temp.add(btn);
+					sp.add(temp.get(i));
+					temp.get(i).setVisible(false);
 					//////////////////////MAYBE SURROUND WITH TRY CATCH
 				}
+				tbuttons.add(temp);
 			}
 		}
 		this.revalidate(); this.repaint();
@@ -117,15 +133,33 @@ public class Bbody extends JPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getSource().getClass().equals(talkbot.Buttons.class)) { int bn;
-			if (on)
+		if (e.getSource().getClass().equals(talkbot.Buttons.class)) {
+			// int bn;
+			/*if (on)
 				bn=1;
 			else
-				bn=2;
+				bn=2; */
 			Buttons temp = (Buttons) e.getSource(); 
-			String tempath = this.con.getPathToAudioFile(bn, temp.getbtnNumber());
+			//bn = temp.getbtnNumber();
+			
+			String tempath = this.con.getPathToAudioFile(currentset+1, temp.getbtnNumber());
 			playSound(tempath);
-		} else if (e.getSource()==set1 && on==false) {
+		} else if (categories.contains((JButton) e.getSource())) {
+			if (currentset != categories.indexOf((JButton) e.getSource())) {
+			for (int i = 0; i < tbuttons.get(currentset).size(); i++) {
+				sp.remove(tbuttons.get(currentset).get(i));
+			}
+			
+			currentset= categories.indexOf((JButton) e.getSource());
+			System.out.println(currentset); 
+			if (!tbuttons.get(currentset).isEmpty()) {
+			for (int i = 0; i < tbuttons.get(currentset).size(); i++) {
+				sp.add(tbuttons.get(currentset).get(i));
+				tbuttons.get(currentset).get(i).addActionListener(this);
+				tbuttons.get(currentset).get(i).setVisible(true);
+			}}
+			this.revalidate(); this.repaint(); }
+		} /*else if (e.getSource()==set1 && on==false) {
 			for (int i = 0; i < track2.size(); i++) {
 				//track2.get(i).setVisible(false); 
 				sp.remove(track2.get(i));
@@ -148,7 +182,7 @@ public class Bbody extends JPanel implements ActionListener {
 				track2.get(i).setVisible(true);
 				track2.get(i).addActionListener(this);
 			} this.revalidate(); this.repaint();
-		}
+		}*/
 	}
 	
 	public void playSound(String temp) {
--- TalkBox/src/talkbot/Configuration.java
+++ TalkBox/src/talkbot/Configuration.java
@@ -6,21 +6,24 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Configuration implements TalkBoxConfiguration{
-//PRACTICE
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2211192959370524557L;
+	//PRACTICE
 	private int totalbtns, aset, audiobtns;
 	//audio and image
 	private String rpathAfile, aname[][], rpathImage[][];
 	//Buttons
 	private String btname[][];
-	protected String[] setNames;
-	private static Configuration config;
+	private ArrayList<String> catnames;
+	private ArrayList<ArrayList<String>> a, rpi, bn;
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
 		ObjectOutputStream obj = new ObjectOutputStream(new FileOutputStream(new File("serial" + File.separator + "log.bin")));
 
-		//Configuration 
-		config = new Configuration();
+		Configuration config = new Configuration();
 		//Test1 before sets
 		/*
 		//Get numbers
@@ -53,27 +56,31 @@ public class Configuration implements TalkBoxConfiguration{
 		*/
 		//Test2 with sets added
 		//Get numbers
-				
 				//config.setAudioSets(2);
-		        config.aset = 2;
-				config.setBtnNum(12);
-				config.review(config.getNumberOfAudioSets(), config.getNumberOfAudioButtons());
+				//config.setBtnNum(12);
+				config.review(2, 12);
 				//AUDIO
-				config.setAudioName(1, 1, "bad.wav");
-				config.setAudioName(1, 2, "first.wav");
-				config.setAudioName(1, 3, "good.wav");
-				config.setAudioName(1, 4, "last.wav");
-				config.setAudioName(1, 5, "little.wav");
-				config.setAudioName(1, 6, "long.wav");
-					config.setAudioName(2, 1, "jealous.wav");
-					config.setAudioName(2, 2, "sad.wav");
-					config.setAudioName(2, 3, "scared.wav");
-					config.setAudioName(2, 4, "suprised.wav");
-					config.setAudioName(2, 5, "angry.wav");
-					config.setAudioName(2, 6, "happy.wav");
+				config.catnames=new ArrayList<String>();
+				config.a = new ArrayList<ArrayList<String>>();
+				config.bn = new ArrayList<ArrayList<String>>();
+				config.rpi = new ArrayList<ArrayList<String>>();
+				config.addcat("Phrases");
+				config.addcat("Emotions");
+				config.Addbtn(1, "Bad", "bad.wav", "Images"+File.separator+"bad.png");//config.setAudioName(1, 1, "bad.wav"); 
+				config.Addbtn(1, "First", "first.wav", "Images"+File.separator+"1st.png");//.setAudioName(1, 2, "first.wav");
+				config.Addbtn(1, "Good", "good.wav", "Images"+File.separator+"good.png");//.setAudioName(1, 3, "good.wav");
+				config.Addbtn(1, "Last", "last.wav", "Images"+File.separator+"last.png");//.setAudioName(1, 4, "last.wav");
+				config.Addbtn(1, "Little", "little.wav", "Images"+File.separator+"little.png");//.setAudioName(1, 5, "little.wav");
+				config.Addbtn(1, "Long", "long.wav", "Images"+File.separator+"long.png");//.setAudioName(1, 6, "long.wav");
+					config.Addbtn(2, "Jealous", "jealous.wav", "Images"+File.separator+"jealous.png");//.setAudioName(2, 1, "jealous.wav");
+					config.Addbtn(2, "Sad", "sad.wav", "Images"+File.separator+"sad.png");//.setAudioName(2, 2, "sad.wav");
+					config.Addbtn(2, "Scared", "scared.wav", "Images"+File.separator+"scared.png");//.setAudioName(2, 3, "scared.wav");
+					config.Addbtn(2, "Suprised", "suprised.wav", "Images"+File.separator+"suprised.png");//.setAudioName(2, 4, "suprised.wav");
+					config.Addbtn(2, "Angry", "angry.wav", "Images"+File.separator+"angry.png");//.setAudioName(2, 5, "angry.wav");
+					config.Addbtn(2, "Happy", "happy.wav", "Images"+File.separator+"happy.png");//.setAudioName(2, 6, "happy.wav");
 				config.setRelativePath("Audio" + File.separator);
 				//Buttons
-				config.setBtnName(1, 1, "Bad");
+				/*config.setBtnName(1, 1, "Bad");
 				config.setBtnName(1, 2, "First");
 				config.setBtnName(1, 3, "Good");
 				config.setBtnName(1, 4, "Last");
@@ -84,10 +91,10 @@ public class Configuration implements TalkBoxConfiguration{
 					config.setBtnName(2, 3, "Scared");
 					config.setBtnName(2, 4, "Suprised");
 					config.setBtnName(2, 5, "Angry");
-					config.setBtnName(2, 6, "Happy");
+					config.setBtnName(2, 6, "Happy");*/
 				config.setTotalBtnNum(14);
 				//Images
-				config.setImagePath(1, 1, "Images" + File.separator + "bad.png");
+				/*config.setImagePath(1, 1, "Images" + File.separator + "bad.png");
 				config.setImagePath(1, 2, "Images" + File.separator + "1st.png");
 				config.setImagePath(1, 3, "Images"+ File.separator +"good.png");
 				config.setImagePath(1, 4, "Images" + File.separator + "last.png");
@@ -98,18 +105,14 @@ public class Configuration implements TalkBoxConfiguration{
 					config.setImagePath(2, 3, "Images" + File.separator + "scared.png");
 					config.setImagePath(2, 4, "Images" + File.separator + "suprised.png");
 					config.setImagePath(2, 5, "Images" + File.separator + "angry.png");
-					config.setImagePath(2, 6, "Images" + File.separator + "happy.png");
+					config.setImagePath(2, 6, "Images" + File.separator + "happy.png");*/
 					//config.Addbtn(1, "test", "Happy.wav", "Images/happy.png");
 					//config.Removebtn(1, 7);
 		//for (int i = 0; i < config.getSet1(); i++) {
 		//	System.out.println(config.getBtnName(1, i+1));
 		//}
-					
-					config.setSetName(0, "Phrases");
-					config.setSetName(1, "Emotions");
-//					System.out.println(config.getSetName(0));
-//					System.out.println(config.getSetName(1));
-					
+			//config.catnames.add("Phrases"); config.catnames.add("Emotions");
+				//config.addcat("Test");
 		obj.writeObject(config);
 		obj.close();
 	}
@@ -192,6 +195,7 @@ public class Configuration implements TalkBoxConfiguration{
 	
 	//Button sets
 	public void setBtnName (int set, int num, String name) {
+		this.a.get(num-1).set(num-1, name);
 		this.btname[set-1][num-1] = name;
 	}
 	
@@ -203,12 +207,27 @@ public class Configuration implements TalkBoxConfiguration{
 		this.totalbtns = num;
 	}
 	
-//	public void setAudioSets (int num) {
-//		this.aset = num;
-//	}
+	public void setAudioSets (int num) {
+		this.aset = num;
+	}
 	
 	public void Removebtn (int set, int num) {
 		set--;
+		//Button Names
+		this.bn.get(set).remove(num-1);
+		this.btname[set] = new String [this.bn.get(set).size()];
+		this.btname[set] = this.a.get(set).toArray(this.btname[set]);
+		
+		//Audio Names
+		this.a.get(set).remove(num-1);
+		this.aname[set] = new String [this.a.get(set).size()];
+		this.aname[set] = this.a.get(set).toArray(this.aname[set]);
+		
+		//Images
+		this.rpi.get(set).remove(num-1);
+		this.rpathImage[set] = new String [this.rpi.get(set).size()];
+		this.rpathImage[set] = this.rpi.get(set).toArray(this.rpathImage[set]);
+		/*
 		ArrayList<String> temp = new ArrayList<String>();
 		for (int i = 0; i < this.btname[set].length; i++) {
 			temp.add(btname[set][i]);
@@ -217,7 +236,6 @@ public class Configuration implements TalkBoxConfiguration{
 		this.btname[set] = new String [temp.size()];
 		this.btname[set] = temp.toArray(this.btname[set]);
 		temp = new ArrayList<String>();
-		
 		for (int i = 0; i < this.aname[set].length; i++) {
 			temp.add(aname[set][i]);
 		}
@@ -225,19 +243,33 @@ public class Configuration implements TalkBoxConfiguration{
 		this.aname[set] = new String [temp.size()];
 		this.aname[set] = temp.toArray(this.aname[set]);
 		temp = new ArrayList<String>();
-		
 		for (int i = 0; i < this.rpathImage[set].length; i++) {
 			temp.add(rpathImage[set][i]);
 		}
-		temp.remove(num-1);
-		this.rpathImage[set] = new String [temp.size()];
-		this.rpathImage[set] = temp.toArray(this.rpathImage[set]);
+		temp.remove(num-1); */
+		//this.rpathImage[set] = new String [temp.size()];
+		//this.rpathImage[set] = temp.toArray(this.rpathImage[set]);
 		this.audiobtns--;
 		this.totalbtns--;
 	}
 	
 	public void Addbtn (int set, String bname, String audioname, String image) {
 		set--;
+		//Button Names
+		this.bn.get(set).add(bname);
+		this.btname[set] = new String [this.bn.get(set).size()];
+		this.btname[set] = this.a.get(set).toArray(this.btname[set]);
+		
+		//Audio Names
+		this.a.get(set).add(audioname);
+		this.aname[set] = new String [this.a.get(set).size()];
+		this.aname[set] = this.a.get(set).toArray(this.aname[set]);
+		
+		//Images
+		this.rpi.get(set).add(image);
+		this.rpathImage[set] = new String [this.rpi.get(set).size()];
+		this.rpathImage[set] = this.rpi.get(set).toArray(this.rpathImage[set]);		
+		/*
 		ArrayList<String> tempb = new ArrayList<String>();
 		ArrayList<String> tempa = new ArrayList<String>();
 		ArrayList<String> tempi = new ArrayList<String>();
@@ -254,13 +286,14 @@ public class Configuration implements TalkBoxConfiguration{
 		this.aname[set] = new String [tempa.size()];
 		this.aname[set] = tempa.toArray(this.aname[set]);
 		this.rpathImage[set] = new String [tempi.size()];
-		this.rpathImage[set] = tempi.toArray(this.rpathImage[set]);
+		this.rpathImage[set] = tempi.toArray(this.rpathImage[set]); */
 		this.audiobtns++; this.totalbtns++;
 	}
 	
 	//Images
 	public void setImagePath (int set, int num, String path) {
 		this.rpathImage[set-1][num-1] = path;
+		this.rpi.get(set-1).set(num-1, path);
 	}
 	
 	//Audio
@@ -269,106 +302,105 @@ public class Configuration implements TalkBoxConfiguration{
 	}
 	
 	public void setAudioName (int set, int num, String name) {
+		//System.out.println("THe data is" +aname[0][0]);
 		this.aname[set-1][num-1] = name;
+		this.a.get(set-1).set(num-1, name);
 	}
 	
 	public void review (int set, int num) {
 		this.aname = new String [set][6];
 		this.btname = new String [set][6];
 		this.rpathImage = new String[set][6];
-		this.setNames = new String[set];
+	}
+	
+	public void addcat(String name) {
+		catnames.add(name);
+		//this.aname[0][0] = "here";
+		//System.out.println("Here      " + this.aname[0][0]);
+		ArrayList<String> tempb = new ArrayList<String>();
+		ArrayList<String> tempa = new ArrayList<String>();
+		ArrayList<String> tempi = new ArrayList<String>();
+		this.a.add(tempa); this.bn.add(tempb); this.rpi.add(tempi);
+		
+		//for (int i = 0; i < a.size(); i++) {
+			//Button Names
+			this.btname = new String [this.bn.size()][];
+			//System.out.printf("[%d][%d]\n", this.bn.size(), this.bn.get(i).size());
+			//Audio Names
+			this.aname = new String [this.a.size()][];
+			//Images
+			this.rpathImage = new String [this.rpi.size()][];
+			//System.out.println(this.btname[i][0] + "\n");
+			
+		//}
+		
+		for (int i = 0; i < this.a.size(); i++) {
+			//Map values to array 
+			System.out.println(a.size());
+			//Button Names
+			this.btname[i] = new String [this.bn.get(i).size()];
+			this.btname[i] = this.a.get(i).toArray(this.btname[i]);
+			//Audio Names
+			this.aname[i] = new String [this.a.get(i).size()];
+			this.aname[i] = this.a.get(i).toArray(this.aname[i]);
+			//Images
+			this.rpathImage[i] = new String [this.rpi.get(i).size()];
+			this.rpathImage[i] = this.rpi.get(i).toArray(this.rpathImage[i]);	
+
+			//}
+		}		
+
+		this.aset++;
+	} 
+	
+	public void removecat(String name) {
+		int num = catnames.indexOf(name);
+		catnames.remove(name);
+		this.a.remove(num); this.bn.remove(num);  this.rpi.remove(num);
+				
+		for (int i = 0; i < a.size(); i++) {
+			//Button Names
+			this.btname = new String [this.bn.size()][this.bn.get(i).size()];
+			//Audio Names
+			this.aname = new String [this.a.size()][this.a.get(i).size()];
+			//Images
+			this.rpathImage = new String [this.rpi.size()][this.rpi.get(i).size()];
+		}
+		
+		//Map values to array
+		this.btname = this.bn.stream().map(u -> u.toArray(new String[0])).toArray(String[][]::new);//this.a.toArray(this.btname);
+		this.aname = this.a.stream().map(u -> u.toArray(new String[0])).toArray(String[][]::new);//this.a.toArray(this.aname);
+		this.rpathImage = this.rpi.stream().map(u -> u.toArray(new String[0])).toArray(String[][]::new);//.toArray(this.rpathImage);
+		
+		this.aset--;
 	}
 
 	protected void removeSetAt(int location)
 	{
-		
-		setNames[location] = null;
-		for(int i = 0; i < 1000; i++)
+//		aname[location]= null;
+//		rpathImage[location] = null;
+//		btname[location] = null;
+		for(int i = 0; i < 6; i++)
 		{
 			try
 			{
-				
 				Removebtn(location, i);
-				System.out.println("Button removed");
 			}
 			catch(IndexOutOfBoundsException E)
 			{
-				System.out.println("Removal out of bounds");
-				//E.printStackTrace();
+				System.out.println("out of bounds");
 			}
 		}
 	}
-	
-	protected String getSetName(int index)
+	protected void addSetAt(int location, String name)
 	{
-		return setNames[index];
+		aname[location][0] = name;
 	}
 
-	protected void setSetName(int set, String name)
-	{
-		try
-		{
-			setNames[set] = name;
+	public String getcatNames (int num) {
+		for (int i = 0; i < catnames.size(); i++) {
+			System.out.println(catnames.get(i));
 		}
-		catch(Exception E)
-		{
-			System.out.println(set);
-			E.printStackTrace();
-		}
-		
+		return this.catnames.get(num);
 	}
-	protected void addSet(String name)
-	{
-	 setNumberPlusOne();
-
-		for(int i = 0; i < getNumberOfAudioSets(); i++)
-		{
-			if (getSetName(i) == null)
-			{
-				setSetName(i, name);
-				System.out.println(name + " " + getSetName(i) + " " + i + " " + getNumberOfAudioSets());
-				break;
-			}
-		}
-		//System.out.println(getSetName(2));
-	}
-	public void setNumberPlusOne() 
-	{
-		int set = getNumberOfAudioSets();
-		String[][] tempAudio = new String[set + 1][];
-		String[][] tempButtonName= new String[set + 1][];
-		String[][] tempImage = new String[set + 1][];		
-		String[] tempSetName = new String[set + 1];
-		
-		for(int i = 0; i < setNames.length; i++)
-		{
-			//for(int j = 0; j )
-			tempAudio[i] = aname[i];
-			tempButtonName[i] = btname[i];
-			tempImage[i] = rpathImage[i];
-			tempSetName[i] = setNames[i];
-		}
-		//System.out.println(aname.length + " " + tempAudio.length);
-		aname = tempAudio;
-		btname = tempButtonName;
-		rpathImage = tempImage;
-		setNames = tempSetName;
-		//System.out.println(aname.length + " " + tempAudio.length);
-		aset++;
-	}
-	
-//	protected void addSetAt(int location, String name)
-//	{
-//		try 
-//		{
-//			btname[location][0] = name;
-//			System.out.println(name);
-//		}
-//		catch(IndexOutOfBoundsException E)
-//		{
-//			System.out.println("New out of bounds");
-//			E.printStackTrace();
-//		}
-//	}
-
 }
