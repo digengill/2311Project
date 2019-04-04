d55249a3d8723b7e9e6a1352f4353f6075a57c57
HP USER <HP USER@DESKTOP-HVLV4NG>
2019-04-04 00:54:46 -0400

    Final Version extra

---
+++ vF/test/TalkBoxSimulatorTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TalkBoxSimulatorTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testTalkBoxSimulator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	 testActionPerformed() {
+		fail("Not yet implemented");
+	}
+
+
+
+}
